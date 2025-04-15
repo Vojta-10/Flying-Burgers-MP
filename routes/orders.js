7465ed781d812376792340d97a0600e87dbf3777
Vojta-10 <vojtasvojto@email.cz>
2025-04-15 23:48:24 +0200

    fix 2

--- routes/orders.js
+++ routes/orders.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { getOrders, makeAnOrder } = require('../controllers/orders');
 
-router.post('/order/make-an-order', makeAnOrder);
-router.get('/order/history', getOrders);
+router.post('/make-an-order', makeAnOrder);
+router.get('/history', getOrders);
 
 module.exports = router;
